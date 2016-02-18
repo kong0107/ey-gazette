106cce38a3273ec0a8a583d76096dd9de94ec33b
Kong Kao <kong0107@gmail.com>
2016-02-19 02:26:24 +0800

    debug

--- .openshift/cron/hourly/updateDatabase.sh
+++ .openshift/cron/hourly/updateDatabase.sh
@@ -12,6 +12,7 @@ xmlFile=${OPENSHIFT_TMP_DIR}$today.xml
 wget http://kong0107.github.io/ey-gazette-data/$year/$today/$today.xml -O $xmlFile
 
 cd $OPENSHIFT_DATA_DIR
+rm import2mongo.js
 wget https://github.com/kong0107/ey-gazette/raw/master/import2mongo.js
 node import2mongo.js $today $xmlFile
 
