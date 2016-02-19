dcd293ecd98bb602a269cc5c7dbbeb7c688afc29
Kong Kao <kong0107@gmail.com>
2016-02-19 22:43:41 +0800

    Update updateDatabase.sh

--- .openshift/cron/hourly/updateDatabase.sh
+++ .openshift/cron/hourly/updateDatabase.sh
@@ -9,7 +9,7 @@ year=$(($(TZ=Asia/Taipei date '+%Y')-1911))
 today=$year-$(TZ=Asia/Taipei date '+%m-%d')
 
 xmlFile=${OPENSHIFT_TMP_DIR}$today.xml
-wget http://kong0107.github.io/ey-gazette-data/$year/$today/$today.xml -O $xmlFile
+wget https://github.com/kong0107/ey-gazette-data/raw/gh-pages/$year/$today/$today.xml -O $xmlFile
 
 cd $OPENSHIFT_DATA_DIR
 rm import2mongo.js
